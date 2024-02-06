99bfa6cc8d38c2387a579318d0ba0a5080d0d474
Sakib Al Islam <73927803+sakib-al@users.noreply.github.com>
2024-02-06 17:09:40 +0600

    complete user api

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.use(logger);
 
@@ -23,6 +27,7 @@ app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
+app.use('/users', require('./routes/userRoutes'))
 
 app.all("*", (req, res) => {
   res.status(404);
@@ -37,4 +42,11 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+
+mongoose.connection.on("error", err => {
+    console.log(err);
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, "mongoErrLog.log")
+})
